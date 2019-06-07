5ad5acf0a2964ae2c91d671ab5afa8d91c342964
y.sasajima <y.sasajima@cb-cloud.com>
2019-06-07 16:37:27 +0900

    asana-count 使用するメソッドの最適化

--- bookmarklet/asana-count/dev/asana-count.js
+++ bookmarklet/asana-count/dev/asana-count.js
@@ -5,10 +5,10 @@
   function r() {
     // カラムを全取得
     const $column = document.querySelectorAll('.BoardBody-column');
-    const columnArr = [].slice.call($column);
+    const columnArr = [...$column];
 
     // DONEのカラムのみを抽出
-    const $targetColumn = columnArr.filter(el => {
+    const $targetColumn = columnArr.find(el => {
       const $title = el.querySelector('.BoardColumnHeaderTitle');
       if(!$title) {
         return false;
@@ -17,8 +17,8 @@
     });
 
     // ポイントを抽出
-    const $targetPill = $targetColumn[0].querySelectorAll('.Pill--small');
-    const pillArr = [].slice.call($targetPill);
+    const $targetPill = $targetColumn.querySelectorAll('.Pill--small');
+    const pillArr = [...$targetPill];
     const pointArr = pillArr.map(v => v.textContent * 1).filter(v => v);
     const count = pointArr.length;
     const points = pointArr.reduce((a, b) => a + b, 0);
